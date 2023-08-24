1698fd824bdec5140059cfe68e8a5fab5bb4724a
lucguara <140097103+lucguara@users.noreply.github.com>
2023-08-23 23:09:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const rainContainer = document.getElementById("rain-container");
-
-function createVector() {
-    const vector = document.createElement("div");
-    vector.classList.add("vector");
-    vector.style.left = `${Math.random() * 100}vw`;
-    vector.style.transform = `rotate(${Math.random() * 360}deg)`; // Set random rotation angle
-    rainContainer.appendChild(vector);
-    setTimeout(() => {
-        vector.remove();
-    }, 5000);
-}
-
-setInterval(createVector, 250); // 4 vectors per second (1000 ms / 4 = 250 ms)
-
 document.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById("video");
     video.play();
+
+    const rainContainer = document.getElementById("rain-container");
+
+    function createVector() {
+        const vector = document.createElement("div");
+        vector.classList.add("vector");
+        vector.style.left = `${Math.random() * 100}vw`;
+        vector.style.backgroundImage = "url('pastel.png')"; // Adjust image path
+        rainContainer.appendChild(vector);
+        setTimeout(() => {
+            vector.remove();
+        }, 5000);
+    }
+
+    setInterval(createVector, 250); // Adjust timing as needed
+});
